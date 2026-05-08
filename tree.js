441d6647934c9c84ad190e1125fc905a33027beb
Nemanja Pavlovic <268587348+dev-nene@users.noreply.github.com>
2026-05-08 13:37:57 +0200

    Implement balanced BST build and prettyPrint

--- tree.js
+++ tree.js
@@ -8,6 +8,29 @@ class Node {
 
 class Tree {
   constructor(arr) {
-    this.root = this.buildTree(arr);
+    const uniq = [...new Set(arr)].sort((a, b) => a - b);
+    this.root = this.#buildTree(uniq);
+  }
+
+  #buildTree(array) {
+    if (array.length === 0) return null;
+
+    const mid = Math.floor(array.length / 2);
+    const root = new Node(array[mid]);
+    root.left = this.#buildTree(array.slice(0, mid));
+    root.right = this.#buildTree(array.slice(mid + 1));
+
+    return root;
+  }
+
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null || node === undefined) return;
+
+    this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 }
+
+const test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+test.prettyPrint();
