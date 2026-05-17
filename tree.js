93363f0302c97d24944a029167b7df1fb231e39b
Nemanja Pavlovic <268587348+dev-nene@users.noreply.github.com>
2026-05-17 23:01:20 +0200

    Add rebalance method

--- tree.js
+++ tree.js
@@ -191,6 +191,16 @@ class Tree {
 
     return this.isBalanced(node.left) && this.isBalanced(node.right);
   }
+
+  rebalance() {
+    const values = [];
+
+    this.inOrderForEach((value) => {
+      values.push(value);
+    });
+
+    this.root = this.#buildTree(values);
+  }
 }
 
 const test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
